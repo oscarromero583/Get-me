db82b59c117e8d641e741151e6a4d699eeefce53
oscarromero583 <obetaoscar583@gmail.com>
2025-01-24 17:38:30 +0100

    new feature - initialized the index Components 	modified:   get-me/src/App.css 	modified:   get-me/src/App.jsx 	modified:   get-me/src/Components/index.css 	modified:   get-me/src/Components/index.jsx 	modified:   get-me/src/Components/logo.css

--- get-me/src/App.jsx
+++ get-me/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Background from "./Components/background";
 import Logo from "./Components/logo";
-import Index from './Components/index';
-import './App.css'
+import Index from "./Components/index"
+import './App.css';
 
 function App() {
-  
-
   return (
-    <>
-    <div className='app-container'>
-      < Background  className='background'/>
-      <Logo className='logo'/>
-      <Index className='index'/>
+    <div className="app-container">
+      <div className="background-container">
+        <Background className="background" />
       </div>
-    </>
-  )
+      <div className="content-container">
+        <Logo className="logo" />
+        <Index className="index" />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- get-me/src/Components/index.jsx
+++ get-me/src/Components/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 import './index.css'
 
 function index() {
   return (
-    <div className='.index'>index</div>
+    <div className='index-content'>
+    <div><h1>Oscar Romero</h1></div>
+    </div>
   )
 }
 
